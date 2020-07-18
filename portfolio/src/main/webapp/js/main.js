66b4af8722dbd47e5ca4a5c030ffb32e6001f31a
Tianyun Zhang <doowzs@outlook.com>
2020-07-18 08:30:54 -0400

    update scripts on page to check user status

--- portfolio/src/main/webapp/js/main.js
+++ portfolio/src/main/webapp/js/main.js
@@ -1,17 +1,45 @@
 // Copyright 2020 Tianyun Zhang
 
-// Fetch JSON array of comments and show then
 const loadComments = () => {
+  fetchComments();
+  fetchUserStatus();
+};
+
+// Fetch JSON array of comments and show them
+const fetchComments = () => {
   fetch('/comments')
     .then(res => res.json())
     .then(comments => {
       element = document.getElementById('comments');
       comments.forEach(comment => {
         node = document.createElement('p');
-        node.innerHTML = comment.name 
+        node.innerHTML = comment.name
+                          + '[' + comment.email + ']'
                           + ' @ ' + comment.date 
                           + ': ' + comment.content;
         element.appendChild(node);
       });
     });
 };
+
+// Fetch user status and show buttons.
+const fetchUserStatus = () => {
+  fetch('/user-status')
+    .then(res => res.json())
+    .then(status => {
+      console.log(status);
+      loginLink = document.getElementById('login-link');
+      logoutLink = document.getElementById('logout-link');
+      emailElement = document.getElementById('user-email');
+      anonymousElement = document.getElementById('new-comment-anonymous');
+      loggedInElement = document.getElementById('new-comment-logged-in');
+      if (status.email) {
+        emailElement.innerHTML = status.email;
+        logoutLink.href = status.logoutUrl;
+        loggedInElement.style["display"] = "block";
+      } else {
+        loginLink.href = status.loginUrl;
+        anonymousElement.style["display"] = "block";
+      }
+    });
+}
